2fec0e0cd3d880c5d2f260d4f27012cb4d3536af
Mustafa Can Öztunç <mustafa.oztunc@ogr.sakarya.edu.tr>
2018-02-01 16:08:31 +0300

    ui-router-rebuilding2

--- app/apps.js
+++ app/apps.js
@@ -8,6 +8,42 @@ angular
                 url:'/',
                 templateUrl:'static/home.html'
             })
+            .state('chprop',{
+                url:'/chprop',
+                templateUrl:'static/chprop.html'
+            })
+            
+            .state('delch',{
+                url:'/delch',
+                templateUrl:'static/delch.html'
+            })
+            
+            .state('logop',{
+                url:'/logop',
+                templateUrl:'static/logop.html'
+            })
+            
+            .state('newch',{
+                url:'/newch',
+                templateUrl:'static/newch.html'
+            })
+            
+            .state('runch',{
+                url:'/runch',
+                templateUrl:'static/runch.html'
+            })
+            
+            .state('startch',{
+                url:'/startch',
+                templateUrl:'static/startch.html'
+            })
+            
+            .state('stopch',{
+                url:'/stopch',
+                templateUrl:'static/stopch.html'
+            })
+            
+
 
 
     }])
